b6ec19bb5bbde9aae59fa4b84668887c04338656
Liam Whelan <liamwhelan08115@gmail.com>
2026-04-28 13:40:24 -0700

    fixed main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-extern crate audio_engine;
-use std::fs::File;
-
-use audio_engine::testing::run_cli_simulation;
-use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
-
 fn main() -> anyhow::Result<()> {
     #[cfg(debug_assertions)]
     {
+        extern crate audio_engine;
+        use audio_engine::testing::run_cli_simulation;
+        use std::fs::File;
+        use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
         let file = File::create("output.log").expect("failed to create log file");
         let file_layer = fmt::layer()
             .with_writer(file)
